fbe69132c45f2a96ab968cd5d1cc60f49097c39a
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-01-12 15:29:22 +0100

    Implement string vec loading -error and print

--- exam-style/exam10.c
+++ exam-style/exam10.c
@@ -80,8 +80,88 @@ The `main` function must:
 * Return `0`.
 */
 
-int main ()
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#define DIM 30
+
+int load_string(int n, char m[n][DIM])
 {
+    int len,i,cond1 = 1,cond2 = 1;
+
+    printf("\nEnter the first string: ");
+    scanf("%s", m[0]);
+
+    len = strlen(m[0]);
+    for(i = 0; i < len-1 && cond1; i++) {
+        if(isupper(m[0][i])) {
+            cond1 = 0; }
+    }
+    for(i = 0; i < len-1 && cond1; i++) {
+        if(m[0][i] > m[0][i+1]) {
+        cond2 = 0; }
+    }
+
+    if(cond1 && cond2) {
+        char k = m[0][len-1];
+        k++;
+
+        for(i = 0; i < n-1; i++) {
+        strcpy(m[i+1], m[i]);
+        m[i+1][len-1] = k;
+        k++; }
+
+        return 1;
+    }
+        else {
+        return 0; }
+}
+
+void print_string(int n, char m[n][DIM])
+{
+    int i,len;
+    
+    len = strlen(m[0]);
+
+    printf("Index:\t");
+    for(i = 0; i < len; i++)
+    {
+        printf("%d\t",i);
+    }
+    printf("\n");
+
+    printf("String:\t");
+    for(i = 0; i < n; i++)
+    {
+        printf("%s\t",m[i]);
+    }
+    printf("\n");
+}
+
+
+int main (int argc, char *argv[])  // ./a n_of_string file_name.txt
+{
+    if(argc != 3) {
+        printf("ERROR 1: wrong number of parameters\n");
+        exit(1); }
+    
+    int n;
+    n = atoi(argv[1]);
+    if(n < 1) {
+        printf("ERROR 2: invalid number of strings\n");
+        exit(2); }
+
+    char m[n][DIM];
+    int res;
+
+    res = load_string(n,m);
+    if(res == 0) {
+    printf("ERROR 3: loading failed\n");
+    exit(3); }
+
+    print_string(n,m);
+
 
     return 0;
 }
