1dded1ada835b0370d4968ed1632e504202191b8
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-03-11 11:28:34 +0100

    Add intro program demonstrating struct definition and input with scanf

---
+++ struct/intro_struct_input.c
@@ -0,0 +1,26 @@
+// Reads a person's basic information using a struct (name, surname, age) 
+// then load the struct with scanf
+#include <stdio.h>
+#include <stdlib.h>
+#define DIM 30
+
+struct personal_info {        
+    char name[DIM];
+    char surname[DIM];
+    int age;
+};
+
+int main ()
+{
+    struct personal_info p;
+
+    printf("Enter your info:\n");
+    printf("Name: ");
+    scanf("%s", p.name);
+
+    printf("\nSurname: ");
+    scanf("%s", p.surname);
+
+    printf("\nAge: ");
+    scanf("%d", &p.age);
+}
