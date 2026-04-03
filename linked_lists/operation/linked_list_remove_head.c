57ef8161dc323819747bbd680a4495a1fd913388
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-04-03 10:27:19 +0200

    implement remove head operation for singly linked list

---
+++ linked_lists/operation/linked_list_remove_head.c
@@ -0,0 +1,76 @@
+// Remove the first node (head) from a singly linked list and update head pointer 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} node;
+ 
+typedef struct node *LINK;  
+
+void append_node(LINK *h);
+void print_list(LINK h);
+void remove_first_node(LINK *h);
+
+int main ()
+{
+    node *head = NULL;
+
+    for(int i = 0; i < 8; i++) {
+    append_node(&head); }
+
+    print_list(head);
+
+    remove_first_node(&head);
+    print_list(head);
+
+    return 0;
+}
+
+void append_node(LINK *h)
+{   
+    node *new_node = malloc(sizeof(*new_node)); 
+    if(new_node == NULL) {
+        printf("Memory fail\n");
+        exit(EXIT_FAILURE); }
+  
+    new_node->data = rand()%10;
+    new_node->next = NULL;
+
+    LINK q = *h;
+
+    if(q == NULL) {
+        *h = new_node;
+        return; }
+
+    while(q->next != NULL) {
+        q = q->next; }
+
+    q->next = new_node;
+}
+
+void print_list(LINK h)
+{
+    LINK q = h;
+
+    while(q != NULL)
+    {
+        printf("node-> %d\t",q->data);
+        q = q->next;
+    }
+}
+
+void remove_first_node(LINK *h)
+{
+
+    if(*h == NULL) {
+        printf("Empty list\n");
+        return; }
+    
+    printf("\n== First node removed ==\n");
+
+    LINK p = *h;
+    *h = p->next;
+    free(p); 
+}
