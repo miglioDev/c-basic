8731ef62f59a734971b0f9c9e8028a27e984ea76
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-04-20 08:39:58 +0200

    Implement positional node insertion

--- linked_lists/operations/linked_list_insert_node_at_position.c
+++ linked_lists/operations/linked_list_insert_node_at_position.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+// Implements insertion of a new node at a specific position in a singly linked list
 #include <time.h>
 
 typedef struct node{
@@ -12,9 +11,11 @@ typedef struct node *LINK;
 LINK new_node();
 void append_node(LINK *head);
 void print_list(LINK l);
+void add_node_in_position(LINK *head, int n, int x);
 
 int main ()
 {
+    int value,position;
     LINK head = NULL;
     srand(time(NULL));
 
@@ -23,6 +24,14 @@ int main ()
         append_node(&head);
     }
 
+    print_list(head);
+
+    printf("\nAdd extra node in position: ");
+    scanf("%d",&position);
+    printf("\nWith value: ");
+    scanf("%d",&value);
+
+    add_node_in_position(&head,position,value); 
     print_list(head);
 
     return 0;
@@ -66,7 +75,33 @@ void print_list(LINK l)
     }
 }
 
-void workin_fun(LINK *head)
+void add_node_in_position(LINK *head, int n, int x)
 {
-    //es.12 core fun
+    int i = 0;
+    LINK node,q = *head;
+    node = new_node();
+    node->data = x;
+
+    if(n < 0) {
+        printf("invalid position\n");
+        exit(1); }
+
+    if(n == 0) {
+        node->next = *head;
+        *head = node;
+        return; }
+
+    while(i < n-1 && q != NULL)
+    {
+        q = q->next;
+        i++;
+    }
+
+    if(q == NULL) {
+        printf("Error - invalid position");
+        free(node);
+        exit(2); }
+
+    node->next = q->next;
+    q->next = node;
 }
