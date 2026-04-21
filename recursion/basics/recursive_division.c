d5daf0cc66f277fd3dde49edda059b98d012120e
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-04-21 18:13:08 +0200

    Add recursive integer division using repeated subtraction

---
+++ recursion/basics/recursive_division.c
@@ -0,0 +1,30 @@
+// recursive integer division via repeated subtraction
+#include <stdio.h>
+
+int division(int x, int y);
+
+int main ()
+{
+    int res,x,y;
+
+    printf("\n== Sum recursive ==\n");
+    printf("\nEnter x: ");
+    scanf("%d",&x);
+
+    printf("\nEnter y: ");
+    scanf("%d",&y);
+
+    res = division(x,y);
+    printf("\nResult = %d",res);
+
+    return 0;
+}
+
+int division(int x, int y)
+{
+    if(x < y) {
+        return 0; }
+    
+    else {
+        return 1 + division(x-y,y); }
+}
