2245ace9202fc616d410ed2f34b4bb564cd6cbc0
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-05-14 17:53:55 +0200

    Add es.c

---
+++ recursion/es.c
@@ -0,0 +1,53 @@
+//
+#include <stdio.h>
+#include <stdlib.h>
+#define DIM 20
+
+typedef struct node{
+    char name[DIM];
+    char surname[DIM];
+    struct node *next;
+}node;
+
+typedef struct node *LINK;
+
+LINK load_list(int *counter);
+
+int main ()
+{
+    int counter;
+    printf("\nEnter list dimension: ");
+    scanf("%d",&counter);
+
+    LINK head = load_list(&counter);
+
+
+    return 0;
+}
+
+LINK new_node() {
+    LINK p = malloc(sizeof(*p));
+
+    if(p == NULL) {
+        printf("Error: memory fail\n");
+        exit(EXIT_FAILURE); }
+    return p;
+}
+
+LINK load_list(int *counter) {
+    LINK p = NULL;
+
+    if(*counter > 0) {
+        (*counter)--;
+        p = new_node();
+
+        printf("\nName: ");
+        scanf("%s",p->name);
+        printf("\nSurname: ");
+        scanf("%s",p->surname);
+
+        p->next = load_list(counter);
+        return p; }
+
+    return p;
+}
