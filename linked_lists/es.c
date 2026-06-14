a7efc5d9f46ddea0e8e0db578f6d217a6b3adb51
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-06-14 18:04:37 +0200

    Add es.c

--- linked_lists/es.c
+++ linked_lists/es.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct node{
     int data;
@@ -9,61 +10,30 @@ typedef struct node{
 typedef struct node *LINK;
 
 LINK new_node();
-void push_node(LINK *head, int x);
-void append_node(LINK *head, int x);
 
 int main ()
 {
-    LINK head = NULL;
+    srand(time(NULL));
 
-    push_node(&head,10);
-    push_node(&head,50);
-    append_node(&head,20);
-    append_node(&head,30);
 
     return 0;
 }
 
 LINK new_node()
 {
-    LINK p = malloc(sizeof(*p));
-
-    if(p == NULL) {
-        printf("Memory fail\n");
+    LINK n = malloc(sizeof(*n));
+    if(n == NULL) {
+        printf("Error: ");
         exit(EXIT_FAILURE); }
-    return p;
+    
+    return n;
 }
 
-void push_node(LINK *head, int x)
-{
-    LINK n = new_node();
-
-    n->data = x;
-    n->next = *head;
-    *head = n;
-}
-
-void append_node(LINK *head, int x)
+void push_node(LINK *head)
 {
     LINK p = new_node();
-    p->data = x;
-    p->next = NULL;
-
-    if(*head == NULL) {
-        *head = p;
-        return; }
-
-    LINK current = *head;
-    while(current->next != NULL) 
-    {
-        current = current->next;
-    }
-
-    current->next = p;
-}
-
-void function()
-{
-    //work
+    p->data = rand()%100;
     
+    p->next = *head;
+    *head = p;
 }
