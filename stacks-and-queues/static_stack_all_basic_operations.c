4684f1171b165859525565788b404b988453043f
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-06-27 19:12:17 +0200

    Implement core operations for static array-based stack

--- stacks-and-queues/static_stack_all_basic_operations.c
+++ stacks-and-queues/static_stack_all_basic_operations.c
@@ -1,3 +1,4 @@
+// Implement a static stack using an array with initialization, push, pop, and top operations
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,22 +10,44 @@ typedef int DATA;
 
 typedef struct STACK{
     DATA s[MAXLEN];
-    DATA top;
+    int top;
 } stack;
 
 void clear(stack *stk);
 int is_empty(stack *stk);
+int is_full(stack *stk);
 int push(stack *stk, int i);
 int top(stack *stk, DATA *out);
 int pop(stack *stk, DATA *out);
 
 int main()
 {
+    int i,n,ex;
     stack s;
     DATA value; 
 
-    
+    clear(&s);
+    printf("How many number will you enter? (n must be < 10):\n\n");
+    scanf("%d",&n);
 
+    for(i = 0; i < n; i++)
+    {
+        printf("\nNumber: ");
+        scanf("%d",&value);
+        push(&s,value);
+    }
+    
+    ex = pop(&s,&value);
+    if(!ex) 
+    printf("Error: unable to execute pop\n");
+        else 
+        printf("Pop executed: value = %d\n",value);
+
+    ex = top(&s,&value);
+    if(!ex)
+    printf("Error: unable to execute top\n");
+        else 
+        printf("Top executed: value = %d\n",value);
 
     return 0;
 }
@@ -39,9 +62,14 @@ int is_empty(stack *stk)
     return(stk->top == EMPTY);   
 }
 
+int is_full(stack *stk)
+{
+    return(stk->top == FULL);
+}
+
 int push(stack *stk, int i)
 {   
-    if(!is_empty(stk)) {
+    if(!is_full(stk)) {
         (stk->top)++;
         stk->s[stk->top] = i;
         return 1;
