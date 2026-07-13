2d764fc3ae6124a8bc259b4027971f55cfe40ec6
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-07-13 18:36:35 +0200

    Update exam04

--- advanced-exam-style/exam04.c
+++ advanced-exam-style/exam04.c
@@ -4,13 +4,13 @@
  * Manage a linked list of integers using text files.
  *
  * a) Write an iterative function:
- *    LINK read_list_from_file(char *filename);
+ *    LINK read_list_from_file(FILE *f);
  *    that reads a sequence of integers from a text file, 
  *    (terminated by the end of the file), builds the corresponding
  *    linked list while preserving the input order (by inserting nodes at
  *    the tail), and returns the head of the list.
  *
- * b) Write an iterative function that finds the smallest value stored
+ * b) Write an iterative function that finds and print the smallest value stored
  *    in the linked list, computes its Fibonacci number, and returns it.
  *    The main function must print the Fibonacci number.
  *
@@ -30,4 +30,137 @@
  *    - writes the filtered list to a file named "output.txt";
  *    - deallocates both linked lists before terminating.
  */
+ 
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node{
+	int d;
+	struct node *next;
+} node;
+
+typedef struct node *LINK;
+
+LINK new_node();
+void append_node(LINK *head, int x);
+LINK read_list_from_file(FILE *fp);
+void print_list(LINK lis);
+int fibonacci_smallest(LINK lis);
+
+int main()
+{
+	FILE *fp = fopen("input.txt", "r");
+	if(fp == NULL) {
+		printf("Error file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	LINK head = read_list_from_file(fp);
+	print_list(head);
+
+	int fib = fibonacci_smallest(head);
+	printf("\nFibonacci = %d\n",fib);
+	
+	return 0;
+}
+
+LINK new_node()
+{
+	LINK p = malloc(sizeof(*p));
+	
+	if(p == NULL) {
+		printf("Error: memory allocation failed\n");
+		exit(EXIT_FAILURE);
+		return p;
+	}
+	return p;
+} 
+ 
+void append_node(LINK *head, int x)
+{
+	LINK p = new_node();
+	p->next = NULL;
+	p->d = x;
+	
+	if(*head == NULL) {
+		*head = p;
+		return;
+	}
+	else {
+		LINK q = *head;
+		while(q->next != NULL) 
+		{
+			q = q->next;
+		}
+		q->next = p;
+	}
+}
+ 
+LINK read_list_from_file(FILE *fp)
+{
+	LINK head,tail,p;
+	int x;
+	head = NULL;
+	
+	while((fscanf(fp,"%d",&x)) == 1)
+	{
+		p = new_node();
+		p->d = x;
+		p->next = NULL;
+		
+		if(head == NULL) {
+			head = p;
+			tail = p;
+		}
+		else {
+			tail->next = p;
+			tail = p;
+		}
+	}
+	
+	return head;
+}
+ 
+void print_list(LINK lis)
+{
+	while(lis != NULL) 
+	{
+		printf("%d ->",lis->d);
+		lis = lis->next;
+	}
+	printf("NULL\n");
+}
+ 
+int fibonacci_smallest(LINK lis)
+{
+	if(lis == NULL) return -1;
+	int number = lis->d;
+	
+	while(lis != NULL)
+	{
+		if(lis->d < number)
+			number = lis->d;
+		
+		lis = lis->next;
+	}
+	printf("Smallest number on the list = %d\n",number);
+	
+	if(number == 0 || number == 1) return number;
+	else {
+	int fib;
+	int fib_minus1 = 1;
+	int fib_minus2 = 0;
+	
+	for(int i = 2; i <= number; i++)
+	{
+		fib = fib_minus1 + fib_minus2;
+		
+		fib_minus2 = fib_minus1;
+		fib_minus1 = fib;
+	}
+	
+	return fib;
+	}
+}
+
+//work in progress
