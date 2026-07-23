aa35caff1c6bf66c883701a79a5048f1f8ad401f
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-07-23 12:25:04 +0200

    Add set up exam09

---
+++ advanced-exam-style/exam09.c
@@ -0,0 +1,23 @@
+// add exam09 text
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	char name[DIM];
+	int age;
+} Element;
+
+typedef struct node{
+	Element d;
+	struct node *next;
+} Node;
+
+typedef struct node *LINK;
+
+int main()
+{
+	LINK head = NULL;
+
+	return 0;
+}
